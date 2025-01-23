04f72c35b89fcaf439c72e81a6a63b5cbffc304a
prosenjeetshil <prosenjeetshil@gmail.com>
2025-01-24 01:23:23 +0530

    feat: styling

--- src/app/components/FrontPageComponent.js
+++ src/app/components/FrontPageComponent.js
@@ -3,8 +3,8 @@ import styles from '../page.module.css';
 const FrontPageComponent = () => {
   return (
     <div className={styles.frontPage}>
-      <h1>Get Your Personalized Diet and Exercise Plan</h1>
-      <p>Start your journey towards a healthier lifestyle with personalized recommendations.</p>
+      {/* <h1>Get Your Personalized Diet and Exercise Plan</h1>
+      <p>Start your journey towards a healthier lifestyle with personalized recommendations.</p> */}
     </div>
   );
 };
